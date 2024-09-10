a2f1243823217f230dfe9a052e8aff0bc89cba60
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-09-10 21:42:35 +0300

    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"

--- Natural_Forest/Natural_Forest/settings_prod.py
+++ Natural_Forest/Natural_Forest/settings_prod.py
@@ -16,5 +16,8 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASS'),
         'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
